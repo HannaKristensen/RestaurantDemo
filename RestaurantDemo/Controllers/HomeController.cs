3fa21faf4107e24f9a81e4d4d9dab80df30bdc49
HannaKristensen <hkristensen15@wou.edu>
2020-05-30 21:22:39 -0700

    Information from Menu to index set up

--- RestaurantDemo/Controllers/HomeController.cs
+++ RestaurantDemo/Controllers/HomeController.cs
@@ -2,19 +2,32 @@
 using RestaurantDemo.Models;
 using System.Linq;
 using System.Web.Mvc;
+using RestaurantDemo.Models.ViewModels;
+using RestaurantDemo.DAL;
 
 namespace RestaurantDemo.Controllers
 {
     public class HomeController : Controller
     {
         private RoleModel db = new RoleModel();
+        private DBContext menuDB = new DBContext();
 
         public ActionResult Index()
         {
             var userID = User.Identity.GetUserId();
             var role = db.AspNetUserRoles.Any(x => x.UserId.ToString() == userID);
             ViewBag.AdminRole = role;
-            return View();
+
+            MenuViewModel model = new MenuViewModel();
+            model.PK = menuDB.Menus.Select(x => x.PK).ToArray();
+            model.Ingredients = menuDB.Menus.Select(x => x.Ingredients).ToArray();
+            model.Price = menuDB.Menus.Select(x => x.Price).ToArray();
+            model.Title = menuDB.Menus.Select(x => x.Title).ToArray();
+            model.Description = menuDB.Menus.Select(x => x.Description).ToArray();
+            model.Catagory = menuDB.Menus.Select(x => x.Catagory).ToArray();
+            model.Avalible = menuDB.Menus.Select(x => x.Avalible).ToArray();
+
+            return View(model);
         }
     }
 }
---
+++ RestaurantDemo/Models/ViewModels/MenuViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantDemo.Models.ViewModels
+{
+    public class MenuViewModel
+    {
+        public int[] PK { get; set; }
+
+        public string[] Title { get; set; }
+
+        public string[] Description { get; set; }
+
+        public string[] Ingredients { get; set; }
+
+        public string[] Catagory { get; set; }
+
+        public decimal[] Price { get; set; }
+
+        public bool[] Avalible { get; set; }
+    }
+}
